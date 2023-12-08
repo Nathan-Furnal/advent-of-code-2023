7c61a70c65e5cced19f8b99534f6574e4b0ad9a5
Nathan Furnal <nathanfurnal.dev@gmail.com>
2023-12-08 10:19:23 +0100

    Since part1 is 'AAA' -> 'ZZZ', it's just the first step of part2

--- day08/day08.py
+++ day08/day08.py
@@ -7,13 +7,7 @@ def solutions():
     dirs, maps = data.split('\n\n')
     maps = {s.split(' = ')[0]: (s.split(' = ')[1].split(', ')[0][1:], s.split(' = ')[1].split(', ')[1][:-1])
             for s in maps.strip().split('\n')}
-    sol1 = 0; pos = 'AAA'
     nodes = [n for n in maps.keys() if n.endswith('A')]; steps = [0] * len(nodes)
-    for d in cycle(dirs):
-        pos = maps[pos][0] if d == 'L' else maps[pos][1]
-        sol1 += 1
-        if pos == 'ZZZ':
-            break
     for i, n in enumerate(nodes):
         pos = n
         for d in cycle(dirs):
@@ -21,8 +15,8 @@ def solutions():
             steps[i] += 1
             if pos.endswith("Z"):
                 break
-        
-    return sol1, math.lcm(*steps)
+            
+    return steps[0], math.lcm(*steps)
 
 if __name__ == "__main__":
     solutions()
